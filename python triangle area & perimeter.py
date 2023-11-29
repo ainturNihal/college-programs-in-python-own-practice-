8e597d8d6e48429b3677cd14f7393858dfd38774
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-29 09:40:53 +0600

    Update python triangle area & perimeter.py

--- python triangle area & perimeter.py
+++ python triangle area & perimeter.py
@@ -1,11 +1,14 @@
-## apython program to find the area and perimeter of a traingle (The user will enter the values)
-
+## a python program to find the area and perimeter of a traingle (The user will enter the values)
+#import time
 print("This a python program that will calculate the area  and perimeter of a triangle from your given values\n\n\n")
+time.sleep(2)
 a = int(input("\nEnter the value of side a: "))
 b = int(input("\nEnter the value of side b: "))
 c = int(input("\nEnter the value of side c: "))
 s = (a+b+c)/2
 area = (s*(s-a)*(s-b)*(s-c))**0.5
+time.sleep(2)
 print(f"\n\n\nThe area of the triangle is: {area:.2f}")
+time.sleep(1)
 print(f"\nThe perimter of the triangle is: {s:.2f}")
 
