d28b34f9c4b761bafa86895ba7c75cc52ac41df3
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-29 12:16:17 +0600

    Add files via upload

---
+++ python prog to see even odd.py
@@ -0,0 +1,8 @@
+## apython program to determine if a number is wheather even or odd.
+
+print("This is a python program to determine the number is even or odd")
+num = int(input("Enter a number: "))
+if num %2 == 0:
+  print(f"The entered number {num} is even.")
+else:
+  print(f"The entered number {num} is odd.")
