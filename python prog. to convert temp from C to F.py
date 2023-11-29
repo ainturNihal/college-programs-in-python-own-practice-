c85511b6e3ebc2471ca3c6da1300e09e03c71ccd
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-11-29 12:06:24 +0600

    Add files via upload

---
+++ python prog. to convert temp from C to F.py
@@ -0,0 +1,6 @@
+## a python program to convert fahrenheit to celsius
+print("This is a python program to convert fahrenheit temprature to celcius")
+
+temp = int(input("Enterthe temprature in celcius: "))
+f = (temp * 9/5) + 32
+print(f"The temprature in fahrenheit is: {f:.2f} F")
