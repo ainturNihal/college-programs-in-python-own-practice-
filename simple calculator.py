17450d743ef861e0a3bebbdb30fc61ada23921c7
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-03 22:51:00 +0600

    Add files via upload

---
+++ simple calculator.py
@@ -0,0 +1,11 @@
+x = int(input("Enter a number for x: "))
+y = (input("Add +,-,*,/: "))
+z = int(input("Enter a number for y: "))
+if y == "+":
+  print(x+z)
+if y == "-":
+  print(x-z)
+if y == "*":
+  print(x*z)
+if y == "/":
+  print(x/z)
