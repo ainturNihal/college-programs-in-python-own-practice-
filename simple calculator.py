ea9b69a3b2c9065be51c16d0d13d510210f7fd33
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-04 23:54:13 +0600

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,11 +1,16 @@
+import time
 x = int(input("Enter a number for x: "))
 y = (input("Add +,-,*,/: "))
 z = int(input("Enter a number for y: "))
 if y == "+":
+  time.sleep(1)
   print(x+z)
 if y == "-":
+  time.sleep(1)
   print(x-z)
 if y == "*":
+  time.sleep(1)
   print(x*z)
 if y == "/":
+  time.sleep(1)
   print(x/z)
