79ba6910bd9ea819e4cdb6740006fa850e75f039
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-05 00:18:15 +0600

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,4 +1,5 @@
 import time
+print("This is a simple calculator to add, subtract, multiply, and divide.")
 x = int(input("Enter a number for x: "))
 y = (input("Add +,-,*,/: "))
 z = int(input("Enter a number for y: "))
