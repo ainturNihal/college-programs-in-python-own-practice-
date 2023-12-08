30fe3b84f2933d4ee915fd6c580c3732dd5a22af
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-08 10:35:55 +0600

    Update simple calculator.py

--- simple calculator.py
+++ simple calculator.py
@@ -1,17 +1,17 @@
 import time
 print("This is a simple calculator to add, subtract, multiply, and divide.")
-x = int(input("Enter a number for x: "))
+x = float(input("Enter a number for x: "))
 y = (input("Add +,-,*,/: "))
-z = int(input("Enter a number for y: "))
+z = float(input("Enter a number for y: "))
 if y == "+":
   time.sleep(1)
-  print(x+z)
+  print(round(x+z))
 if y == "-":
   time.sleep(1)
-  print(x-z)
+  print(round(x-z))
 if y == "*":
   time.sleep(1)
-  print(x*z)
+  print(round(x*z))
 if y == "/":
   time.sleep(1)
-  print(x/z)
+  print(round(x/z))
