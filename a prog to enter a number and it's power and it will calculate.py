172296f8e2d44c125a273854519669efe96ba612
ainturNihal <151900700+ainturNihal@users.noreply.github.com>
2023-12-16 23:52:57 +0600

    Add files via upload

---
+++ a prog to enter a number and it's power and it will calculate.py
@@ -0,0 +1,5 @@
+#a pthon program to ener a number and it's power and find the result
+n = int(input("Enter a number = "))
+t = int(input("Enter it's power = "))
+v = n**t
+print(f"Entered number's result = {v}")
